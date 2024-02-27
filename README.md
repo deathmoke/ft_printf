97e06eaa2313a7d4c27947ac4f0878ea5e4c46e8
deathmoke <celian.cadoret@gmail.com>
2024-02-27 10:59:45 +0100

    new repo

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ft_printf
 ft_printf
+my ft_printf is basic, but work and he's unbreakable, like the diamond
---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hprintf.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ccadoret <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 12:52:07 by ccadoret          #+#    #+#             */
+/*   Updated: 2023/10/21 12:52:09 by ccadoret         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_printf(const char *format, ...);
+int	ft_putnbr_base(unsigned int nbr, char *base);
+int	ft_putstr(char *str);
+int	ft_putchar(char c);
+int	ft_putptr(void *ptr);
+int	ft_strlen(char *str);
+int	ft_putnbr(int nb);
